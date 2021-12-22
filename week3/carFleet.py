53a09fd508b2ec12d5e56bee15a2acfe4295fa11
Samuel Birhanu <samuelbirhanu121@gmail.com>
2021-12-22 19:24:58 +0300

    adding files

---
+++ week3/carFleet.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        time_left = []
+        data = sorted([(position[i], speed[i]) for i in range(len(position))])
+        
+        print(data)
+        
+        for i in range(len(position)):
+            time_left.append((target-data[i][0])/data[i][1])
+            
+        stack = []
+        
+        for item in time_left:
+            while(len(stack) != 0 and item >= stack[-1]):
+                stack.pop()
+            stack.append(item)
+            
+        return len(stack)
--- week3/maximumNumberOfCoins.py
+++ week3/maximumNumberOfCoins.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numIdenticalPairs(self, nums: List[int]) -> int:
+    def numIdenticalPairs(self, nums: list[int]) -> int:
         counter = 0
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
