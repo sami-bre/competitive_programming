5eed888ba2d12ba62ec8214fced01a9f844e623a
Samuel Birhanu <samuelbirhanu121@gmail.com>
2021-12-24 02:13:35 +0300

    adding a file

--- week3/designCircularDeque.py
+++ week3/designCircularDeque.py
@@ -1,38 +1,74 @@
 class MyCircularDeque:
+    
 
     def __init__(self, k: int):
-        self.deque = []
-        self.max_size = k
+        self.f = []
+        self.b = []
         self.size = 0
-
+        self.max_size = k
+    
+    # two utility functions
+    def f_give_rear(self, pop):
+        for i in range(len(self.f)-1):
+            self.f.append(self.f.pop(0))
+        temp = self.f.pop(0)
+        if(pop):
+            return temp
+        else:
+            self.f.append(temp)
+            return temp
+        
+    def b_give_rear(self, pop):
+        r = self.b
+        for i in range(len(self.b)-1):
+            self.b.append(self.b.pop(0))
+        temp = self.b.pop(0)
+        if(pop):
+            return temp
+        else:
+            self.b.append(temp)
+            return temp
+        
+    ##############################################
     def insertFront(self, value: int) -> bool:
         if(self.size < self.max_size):
-            self.deque.insert(0, value)
+            self.f.append(value)
             self.size += 1
             return True
         else:
             return False
+        
+        
+        
 
     def insertLast(self, value: int) -> bool:
         if(self.size < self.max_size):
-            self.deque.append(value)
+            self.b.append(value)
             self.size += 1
-            return True
-        else:
-            return False
-
-    def deleteFront(self) -> bool:
-        if(self.size > 0):
-            self.deque.pop(0)
-            self.size -= 1
             return True
         else:
             return False
         
 
+    def deleteFront(self) -> bool:
+        if(self.f):
+            self.f_give_rear(True)
+            self.size -= 1
+            return True
+        elif(self.b):
+            self.b.pop(0)
+            self.size -= 1
+            return True
+        else:
+            return False
+
     def deleteLast(self) -> bool:
-        if(self.size > 0):
-            self.deque.pop()
+        if(self.b):
+            self.b_give_rear(True)
+            self.size -= 1
+            return True
+        elif(self.f):
+            self.f.pop(0)
             self.size -= 1
             return True
         else:
@@ -40,29 +76,35 @@ class MyCircularDeque:
         
 
     def getFront(self) -> int:
-        if(self.size > 0):
-            return self.deque[0]
-        return -1
+        if(self.f):
+            return self.f_give_rear(False)
+        elif(self.b):
+            return self.b[0]
+        else:
+            return -1
         
 
     def getRear(self) -> int:
-        if(self.size > 0):
-            return self.deque[-1]
+        if(self.b):
+            return self.b_give_rear(False)
+        elif(self.f):
+            return self.f[0]
         else:
             return -1
-            
         
 
     def isEmpty(self) -> bool:
         if(self.size == 0):
             return True
-        return False
+        else:
+            return False
         
 
     def isFull(self) -> bool:
         if(self.size == self.max_size):
             return True
-        return False
+        else:
+            return False
         
 
 
