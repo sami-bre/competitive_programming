767589b7a696ed21c27f6c5939595ca5a45a2a23
Samuel Birhanu <samuelbirhanu121@gmail.com>
2022-01-23 10:18:43 +0300

    adding a file

---
+++ week5/K-th_symbol_in_grammar.py
@@ -0,0 +1,34 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        N =  n
+        n = 2
+        num = "0"
+        
+        
+        
+        def getNumber(N, num, n, k):
+            if N == 1:
+                return 0
+            
+            if n == N+1: #base case
+                return int(num)
+            else:
+                if num=="0":
+                    num =  "01"
+                elif num=="1":
+                    num = "10"
+                else:
+                    print("There is a problem")
+                
+                if k > (2**(N-1))/(2**(n-1)):
+                    k -= (2**(N-1))/(2**(n-1))
+                    num = num[1]
+                else:
+                    num = num[0]
+                    
+                n += 1
+                
+            return getNumber(N, num, n, k)
+        
+        return getNumber(N, num, n, k)
+                
